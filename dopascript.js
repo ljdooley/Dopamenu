ae8799dfdb9bf61544db12cc233de3bbacb530b6
Lauren Dooley <65740906+ljdooley@users.noreply.github.com>
2020-06-23 06:01:20 -0400

    Add JS to expand and collapse with for loop

--- dopascript.js
+++ dopascript.js
@@ -1,1 +1,14 @@
 // collapse and expand MenuOptions
+let collapsible = document.getElementsByClassName("collapse"); 
+
+for(let i = 0; i < collapsible.length; i++){
+    collapsible[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block"){
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
