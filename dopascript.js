6ec42d5d1ef52fcfaae732c19e1654b3b459af5e
Lauren Dooley <65740906+ljdooley@users.noreply.github.com>
2020-07-01 18:42:25 -0400

    Add media query

--- dopascript.js
+++ dopascript.js
@@ -1,14 +1,33 @@
-// collapse and expand MenuOptions
-let collapsible = document.getElementsByClassName("collapse"); 
-
-for(let i = 0; i < collapsible.length; i++){
-    collapsible[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if (content.style.display === "block"){
-            content.style.display = "none";
+function mediaspecific(x) { 
+    let collapsible = document.getElementsByClassName("collapse"); 
+    let i;
+    for(i = 0; i < collapsible.length; i++){
+        collapsible[i].nextElementSibling.setAttribute("id", `${i}`)
+        let content = document.getElementById(i);
+        if (x.matches) {
+            collapsible[i].removeAttribute("onclick");
+            collapsible[i].disable = "true";
+            content.style.display= "block";
         } else {
-            content.style.display = "block";
+            collapsible[i].setAttribute("onclick", `accordion(${i})`);
+            content.style.display = "none";
         }
-    });
+    }
 }
+
+function accordion(x) {
+    let content = document.getElementById(x)
+    //console.log(content);
+    if (content.style.display === "block"){
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+        
+    }
+}
+
+var notmobile = window.matchMedia("(min-width:600px)");
+mediaspecific(notmobile);
+notmobile.addListener(mediaspecific);
+
+
